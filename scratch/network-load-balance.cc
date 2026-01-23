c95b50fda11732649c04e3b3266f338e92e077b0
Bluecath <57232871@qq.com>
2026-01-23 04:08:24 +0000

    update fat_k8_320_100G_OS2
    Signed-off-by: Bluecath <57232871@qq.com>

--- scratch/network-load-balance.cc
+++ scratch/network-load-balance.cc
@@ -1389,7 +1389,7 @@ int main(int argc, char *argv[]) {
     std::map<std::string, uint32_t> topo2bdpMap;
     topo2bdpMap[std::string("leaf_spine_128_100G_OS2")] = 104000;  // RTT=8320
     topo2bdpMap[std::string("fat_k8_100G_OS2")] = 156000;      // RTT=12480 --> all 100G links, 256 hosts
-    topo2bdpMap[std::string("fat_k8_320_100G_OS2")] = 182000;  // RTT=14560 --> all 100G links, 320 hosts
+    topo2bdpMap[std::string("fat_k8_320_100G_OS2")] = 156000;  // RTT=12480 --> all 100G links, 320 hosts, 16 core switches
 
     // topology_file
     bool found_topo2bdpMap = false;
